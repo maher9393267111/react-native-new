639214e70ed727ec1c94aa5200acba383b3123dc
maher <gomemahero.com>
2022-10-02 20:10:45 +0300

    add expo icons in native cli project

--- screens/Home.js
+++ screens/Home.js
@@ -8,15 +8,17 @@ import { useNavigation } from "@react-navigation/native";
 
 //import {firestore} from './firebase/index'
 import firestore from '@react-native-firebase/firestore';
-import {NativeBaseConfigProvider} from 'native-base';
 
-import Icon from "react-native-vector-icons/Ionicons";
+
+//import Icon from "react-native-vector-icons/Ionicons";
 
 import {NativeBaseProvider} from 'native-base';
 import {LogBox} from 'react-native';
 
 import {Avatar} from 'react-native-paper';
 //import { Assets , Image  , Button} from "react-native-ui-lib";
+import { Button, FormControl, HStack, Input, VStack, Icon } from 'native-base';
+import { Ionicons } from '@expo/vector-icons';
 
 
 
@@ -39,9 +41,16 @@ export default function Home() {
   return (
     <View>
       
-<Text className="  text-blue-600 text-2xl text-center font-bold">hello {curId}</Text>
+<Text className="  text-blue-600 text-2xl text-center font-bold">hellos {curId}</Text>
 
-
+<HStack width='100%' space={2}>
+                    <Input placeholder='movie' variant='filled' bg='gray.200' px={3} width='85%'
+                    InputLeftElement={
+                        <Icon size={5} ml={2} color='gray.400' as={<Ionicons name="ios-search"/> } />
+                    } 
+                    /> 
+                         <Button  startIcon={<Icon as={Ionicons} name='ios-search'/>}>Search</Button>
+                </HStack>
 
          
     </View>
